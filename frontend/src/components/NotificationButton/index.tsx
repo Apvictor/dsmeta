8a7c163bf8ff00d4c3c60d14e79d58023cf4bc96
Armando Víctor Pereira <armandinho14.ap@gmail.com>
2022-07-12 14:45:40 -0300

    Other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -3,13 +3,11 @@ import './styles.css'
 
 function NotificationButton() {
     return (
-        <>
-            <div className="dsmeta-red-btn-container">
-                <div className="dsmeta-red-btn">
-                    <img src={icon} alt="Notificar" />
-                </div>
+        <div className="dsmeta-red-btn-container">
+            <div className="dsmeta-red-btn">
+                <img src={icon} alt="Notificar" />
             </div>
-        </>
+        </div>
     )
 }
 
