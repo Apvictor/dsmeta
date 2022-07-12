7287f080a12657c16ce47faf05d3b6f7a202be39
Armando Víctor Pereira <armandinho14.ap@gmail.com>
2022-07-12 15:44:43 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import logo from '../../assets/img/logo.svg'
-import './styles.css'
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -16,4 +16,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
